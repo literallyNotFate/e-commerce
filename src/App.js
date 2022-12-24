5ce4b46944e81c668d8f60be6efb3119650be789
Fate <dxfinitelynotfate@gmail.com>
2022-12-24 16:10:52 +0200

    Routing setting, importing all components/pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import Feedback from "./pages/Feedback";
+// Importing routes
+import {BrowserRouter as Router, useRoutes} from 'react-router-dom';
+
+// Pages
+import About from './pages/About';
+import Contacts from './pages/Contacts';
+import Feedback from './pages/Feedback';
+
+// Components
+import Header from './components/Header';
+import Products from './components/Products';
+import Footer from './components/Footer';
+
+// Styles for div
+const mainContentStyle = {
+  width:'81%',
+  marginLeft:'17.5%',
+  marginBottom:'15em',
+};
+
+// Setting routes
+const AppRoutes = () => {
+  let routes = useRoutes([
+    {path: "/", element: <Products/>},
+    {path:"/about", element: <About/>},
+    {path: "/contacts", element: <Contacts/>},
+    {path: "/feedback", element: <Feedback/>},
+  ]);
+  return routes;
+}
 
 // Main
 export default function App() {
-    return (
+  return (
       <>
-        <Feedback/>
+        <Router>
+          <Header/>
+          <div style={mainContentStyle}>
+            <AppRoutes/>
+          </div>
+          <Footer/>
+        </Router>
       </>
-    );
+  );
 }
